263af75696f9ba61d75178d3783e830922ea172a
MircaSalvatorul <mateimircea919@gmail.com>
2025-05-10 16:22:28 +0300

    Active list

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,34 @@
+//empty brackets mean auto-fragments
+//dc fragments? so you dont put an extra div inside the DOM
+
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["London", "San Francisco", "Tokyo", "New York", "Paris"];
+  
+  //hook
+  const [selectedIndex, setselectedIndex]= useState(-1)
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setselectedIndex(index)}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
